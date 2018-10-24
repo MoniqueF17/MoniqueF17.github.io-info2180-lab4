b20e160d3c8ad6d65d183231960625868e3e6a0f
MoniqueF17 <moniquefrancis@live.com>
2018-10-23 21:12:57 -0500

    Update maze.js

--- maze.js
+++ maze.js
@@ -3,9 +3,9 @@ var loser = false;  // if the user touches a wall
 function startClick() {
     "use strict";
     loser = false;
-    $("status").textContent = "Find the end!";
+    ("status").textContent = "Find the end!";
     var i, boundaries;
-    boundaries  = $$("div#maze div.boundary");
+    boundaries  = ("div#maze div.boundary");
     for (i = 0; i < boundaries.length; i += 1) {
         boundaries[i].removeClassName("you lose");
     }
@@ -14,7 +14,7 @@ function startClick() {
 function overEnd() {
     "use strict";
     if (!loser) {
-        $("status").textContent = "You win! :]";
+        ("status").textContent = "You win! :]";
     }
 }
 
@@ -22,8 +22,8 @@ function overBoundary() {
     "use strict";
     var i, boundaries;
     loser = true;
-    $("status").textContent = "You lose!";
-    boundaries = $$("div#maze div.boundary");
+    ("status").textContent = "You lose!";
+    boundaries = ("div#maze div.boundary");
     for (i = 0; i < boundaries.length; i += 1) {
         boundaries[i].addClassName("youlose");
     }
@@ -36,9 +36,9 @@ function window() {
 window.onload = function () {
     "use strict";
     var i, boundaries;
-    $("start").onclick = startClick;
-    $("end").onmouseover = overEnd;
-    boundaries = $$("div#maze div.boundary");
+    ("start").onclick = startClick;
+    ("end").onmouseover = overEnd;
+    boundaries = ("div#maze div.boundary");
     for (i = 0; i < boundaries.length; i += 1) {
         boundaries[i].onmouseover = overBoundary;
     }
