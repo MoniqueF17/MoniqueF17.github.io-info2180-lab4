cd403f5d7a895b06cc1a4fbc99496514ae000ca8
MoniqueF17 <moniquefrancis@live.com>
2018-10-23 20:49:20 -0500

    Update maze.js

--- maze.js
+++ maze.js
@@ -1,34 +1,45 @@
-var loser = false;  // whether the user has hit a wall
-
-window.onload = function() {
-    $("start").onclick = startClick;
-    $("end").onmouseover = overEnd;
-    var boundaries = $$("div#maze div.boundary");
-    for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].onmouseover = overBoundary;
-    }
-};
-
-function overBoundary() {
-    loser = true;
-    var boundaries = $$("div#maze div.boundary");
-    for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].addClassName("youlose");
-    }
-}
+var loser = false;  // if the user touches a wall
 
 function startClick() {
+    "use strict";
     loser = false;
-    var boundaries = $$("div#maze div.boundary");
-    for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].removeClassName("youlose");
+    ("status").textContent = "Find the end!";
+    var i, boundaries;
+    boundaries  = ("div#maze div.boundary");
+    for (i = 0; i < boundaries.length; i) {
+        boundaries[i].removeClassName("you lose");
     }
 }
 
 function overEnd() {
-    if(loser) {
-        alert("Sorry, you lost. :[");
-    } else {
-        alert("You win! :]");
+    "use strict";
+    if (!loser) {
+        ("status").textContent = "You win! :]";
     }
 }
+
+function overBoundary() {
+    "use strict";
+    var i, boundaries;
+    loser = true;
+    ("status").textContent = "You lose!";
+    boundaries = ("div#maze div.boundary");
+    for (i = 0; i < boundaries.length; i) {
+        boundaries[i].addClassName("youlose");
+    }
+}
+
+function window() {
+    "use strict";
+}
+
+window.onload = function () {
+    "use strict";
+    var i, boundaries;
+    ("start").onclick = startClick;
+    ("end").onmouseover = overEnd;
+    boundaries = ("div#maze div.boundary");
+    for (i = 0; i < boundaries.length; i) {
+        boundaries[i].onmouseover = overBoundary;
+    }
+};
